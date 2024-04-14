c77c7c901d925dce0e2fe9351998dd08ce7d9f30
zerosicx <hcsofteng@gmail.com>
2024-04-15 10:45:00 +1200

    Make basic components

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Card, CardContent } from "@mui/material";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const MainPage = (props: Props) => {
   return (
-    <div>
-        Main Page
+    <div className="w-full flex justify-center">
+      <Button variant="default" size="lg">
+        Default Button
+      </Button>
+      <Button variant="destructive" size="sm">
+        Default Button
+      </Button>
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
